f231c900014101ef73cc32105fb7e7eba4b79fa3
AuxXxilium <info@auxxxilium.tech>
2024-10-12 21:22:37 +0200

    arc: rework sanity check
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- system/arc.sh
+++ system/arc.sh
@@ -327,12 +327,6 @@ function arcVersion() {
       ARCMODE="config"
     fi
     writeConfigKey "arc.mode" "${ARCMODE}" "${USER_CONFIG_FILE}"
-  elif [ "${ARCMODE}" == "automated" ] || [ "${ARCRESTORE}" == "true" ]; then
-    PAT_URL="$(readConfigKey "paturl" "${USER_CONFIG_FILE}")"
-    PAT_HASH="$(readConfigKey "pathash" "${USER_CONFIG_FILE}")"
-    VALID="true"
-  fi
-  if [ "${ARCMODE}" != "automated" ];  then
     # Check PAT URL
     dialog --backtitle "$(backtitle)" --colors --title "DSM Version" \
       --infobox "Check PAT Data..." 3 40
@@ -345,6 +339,8 @@ function arcVersion() {
       writeConfigKey "paturl" "${PAT_URL}" "${USER_CONFIG_FILE}"
       writeConfigKey "pathash" "${PAT_HASH}" "${USER_CONFIG_FILE}"
     fi
+  elif [ "${ARCMODE}" == "automated" ] || [ "${ARCRESTORE}" == "true" ]; then
+    VALID="true"
   fi
   # Check PAT Hash
   PAT_URL="$(readConfigKey "paturl" "${USER_CONFIG_FILE}")"
